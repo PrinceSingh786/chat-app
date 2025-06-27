9eec8f65e159d7423ad06ef424e6d5647168c3b7
Prince Singh <psjeee8@gmail.com>
2025-06-27 18:38:25 +0530

    Add all files

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,104 @@
+import React, { useContext, useState } from "react";
+import assets from "../assets/assets";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+
+const Profile = () => {
+  const { authUser, updateProfile } = useContext(AuthContext);
+
+  const [selectedImg, setSelectedImg] = useState(null);
+  const navigate = useNavigate();
+  const [name, setName] = useState(authUser.fullName);
+  const [bio, setBio] = useState(authUser.bio);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!selectedImg) {
+      await updateProfile({ fullName: name, bio });
+      navigate("/");
+      return;
+    }
+    const reader = new FileReader();
+    reader.readAsDataURL(selectedImg);
+    reader.onload = async () => {
+      const base64Image = reader.result;
+      await updateProfile({ profilePic: base64Image, fullName: name, bio });
+      navigate("/");
+    };
+  };
+
+  return (
+    <div className="min-h-screen bg-cover bg-no-repeat flex items-center justify-center">
+      <div className=" w-5/6 max-w-2x1 backdrop-blur-2x1 text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-1g">
+        {/* ------------left side profile update */}
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-5 p-10 flex-1"
+        >
+          <h3 className="text-lg ">Profile Details</h3>
+          <label
+            htmlFor="avatar"
+            className="flex items-center cursor-pointer gap-3"
+          >
+            <input
+              type="file"
+              onChange={(e) => setSelectedImg(e.target.files[0])}
+              id="avatar"
+              accept=".png, .jpg, .jpeg"
+              hidden
+            />
+            <img
+              src={
+                selectedImg
+                  ? URL.createObjectURL(selectedImg)
+                  : assets.avatar_icon
+              }
+              className={`w-12 h-12 ${selectedImg && "rounded-full"}`}
+              alt=""
+            />
+            upload profile image
+          </label>
+
+          <input
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            type="text"
+            required
+            placeholder="Your name "
+            className="p-2 border border-gray-500 rounded-md
+          focus:outline-none focus:ring-2 focus:ring-violet-500"
+          />
+          <textarea
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+            placeholder="Write Profile bio"
+            required
+            rows={4}
+            className="p-2 border border-gray-500 rounded-md
+          focus:outline-none focus:ring-2 focus:ring-violet-500"
+          ></textarea>
+          <button
+            type="submit"
+            className="bg-gradient-to-r from-purple-400 <
+
+26
+
+to-violet-600 text-white p-2 rounded-full text-Ig cursor-pointer"
+          >
+            Save
+          </button>
+        </form>
+        {/* ----------right side only image */}
+        <img
+          src={authUser.profilePic || assets.logo_icon}
+          alt=""
+          className={`max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10 ${
+            selectedImg && "rounded-full"
+          }`}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
